3867bc90a3041de6a4902a130e3ebaedf3520bda
plee19 <42356593+plee19@users.noreply.github.com>
2018-09-03 14:10:38 -0500

    AnimalIO tests created

---
+++ src/us/plee19/test/AnimalIOTest.java
@@ -0,0 +1,30 @@
+package us.plee19.test;
+
+import org.junit.*;
+import us.plee19.Cat;
+import us.plee19.Dog;
+import us.plee19.Student;
+
+public class AnimalIOTest extends junit.framework.TestCase {
+    String name = "Kitty";
+    int miceKilled = 10;
+    String name2 = "Fido";
+    Boolean isFriendly = true;
+    int age = 21;
+    String name3 = "Paul";
+
+    @Test
+    public void testAddCat() throws Exception {
+        assertTrue(new Cat(miceKilled, name) instanceof Cat);
+    }
+
+    @Test
+    public void testAddDog() throws Exception {
+        assertTrue(new Dog(isFriendly, name2) instanceof Dog);
+    }
+
+    @Test
+    public void testAddStudent() throws Exception {
+        assertTrue(new Student(age, name) instanceof Student);
+    }
+}
